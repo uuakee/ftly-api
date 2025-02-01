ecd4dece059b56d90fccb086a1d7c84bc56bc90a
joao-gallo <contato@kaueguedes.com.br>
2025-02-01 05:33:46 -0300

    update server production

--- src/server-prod.js
+++ src/server-prod.js
@@ -13,5 +13,5 @@ app.use('/api/auth', authRoutes);
 app.use('/api/school', schoolRoutes);
 app.use('/api/course', courseRoutes)
 
-const PORT = process.env.PORT || 3000;
+const PORT = 1938;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
